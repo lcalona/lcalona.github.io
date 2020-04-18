1b91023e66ef58bf051ba651d9f481ce9d239f53
Luis Calona <calona.luis@gmail.com>
2020-04-18 16:33:35 -0700

    Updates

---
+++ precache-manifest.0b59f3ae8e633dbb517402138d741438.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bf2d6063b9162cad8fe323aa61225fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb7495c64b6b08c92c05",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7e1adea6dd7d4720bee3",
+    "url": "/static/js/2.3f9646af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3f9646af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb7495c64b6b08c92c05",
+    "url": "/static/js/main.2e472746.chunk.js"
+  },
+  {
+    "revision": "817d2bf81990a1d39ee2",
+    "url": "/static/js/runtime-main.6537f79a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
