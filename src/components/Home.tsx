6e476661feec258fda16b746eb2730355aecbf8f
Luis Calona <calona.luis@gmail.com>
2020-05-17 20:56:40 -0700

    Added navbar & updated styling

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,53 +1,77 @@
-import React from 'react';
+import React from "react";
 
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Card from 'react-bootstrap/Card';
-
-import SchoolProven from '../img/SchoolProven.png';
-import TechOut from '../img/TechOut.png';
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
 
 export default function Home() {
+  const SchoolProven = require("../img/SchoolProven.png");
+  const TechOut = require("../img/TechOut.png");
+
+  const title = (
+    <>
+      <h2>Luis Calona</h2>
+      <i>Software Engineer</i>
+    </>
+  );
+
   return (
-    <Container className="Home">
-      <p>
-        Hi, I'm Luis. I'm currently a senior at UC San Diego studying Computer Science with a
-        minor in Design. I will be joining Quizlet full-time in the fall. I've previously had
-        internships at Chan Zuckerberg Initiative, Dropbox, and Bentley Systems.
-      </p>
+    <>
+      <Container className="Home">
+        <Navbar sticky="top" bg="white">
+          <Navbar.Brand href="#home">{title}</Navbar.Brand>
+          <Nav>
+            <Nav.Link>Projects</Nav.Link>
+            <Nav.Link>About Me</Nav.Link>
+          </Nav>
+        </Navbar>
 
-      <div className="Home-projects">
-        <h2 className="Home-projects-title">My Projects</h2>
-        <Row>
-          <Col>
-            <Card>
-              <Card.Body>
-                <Card.Img src={SchoolProven} />
-                <Card.Title>SchoolProven</Card.Title>
-                <Card.Body>
-                  A web application aimed at providing students with a comprehensive tool
-                  for researching their classes.
-                </Card.Body>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card>
-              <Card.Body>
-                <Card.Img src={TechOut} />
-                <Card.Title>TechOut</Card.Title>
-                <Card.Body>
-                  An interactive kiosk that aims to raise awareness on Geisel Library’s Tech Lending
-                  Program and make its equipment more accessible to UCSD students by creating a
-                  frictionless process for checking out equipment, during, and after work hours.
-                </Card.Body>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </div>
+        <br />
+        <br />
 
-    </Container>
+        <p className="Home-intro">
+          Hi, I'm Luis. I'm currently a senior at UC San Diego studying Computer
+          Science with a minor in Design. I will be joining Quizlet full-time in
+          the fall. I've previously had internships at Chan Zuckerberg
+          Initiative, Dropbox, and Bentley Systems.
+        </p>
+
+        <div className="Home-projects">
+          <h2 className="Home-projects-title">My Projects</h2>
+          <Row>
+            <Col>
+              <Card>
+                <Card.Body>
+                  <Card.Img src={SchoolProven} />
+                  <Card.Title>SchoolProven</Card.Title>
+                  <Card.Body>
+                    A web application aimed at providing students with a
+                    comprehensive tool for researching their classes.
+                  </Card.Body>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card>
+                <Card.Body>
+                  <Card.Img src={TechOut} />
+                  <Card.Title>TechOut</Card.Title>
+                  <Card.Body>
+                    An interactive kiosk that aims to raise awareness on Geisel
+                    Library’s Tech Lending Program and make its equipment more
+                    accessible to UCSD students by creating a frictionless
+                    process for checking out equipment, during, and after work
+                    hours.
+                  </Card.Body>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      </Container>
+    </>
   );
 }
