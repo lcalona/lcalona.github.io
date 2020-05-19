e7444d4b5556c2103de5cd32c3ffdff776018838
Luis Calona <calona.luis@gmail.com>
2020-05-18 18:56:54 -0700

    Added ScrollToTop

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import { HashRouter } from "react-router-dom";
 
 import './index.css';
 import App from './App';
+import ScrollToTop from './util/ScrollToTop';
 
 ReactDOM.render(
   <React.StrictMode>
     <HashRouter basename="/">
+      <ScrollToTop />
       <App />
     </HashRouter>
   </React.StrictMode>,
